e5e242f9c74849d8854f73695bb9808a3584e5d7
kamil paczkowski <pivotguy888@gmail.com>
2023-09-21 17:25:40 +0200

    Change MainPage header to Wallets, made MainPage empty

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,17 +8,5 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count = count + 10;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
