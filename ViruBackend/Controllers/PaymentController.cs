8778eeb918b6e6a9265cc301e469b64838d83c08
kamil paczkowski <pivotguy888@gmail.com>
2023-10-27 19:56:30 +0200

    Add PaymentController

---
+++ ViruBackend/Controllers/PaymentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViruBackend.Dto;
+using ViruBackend.Models;
+
+
+namespace ViruBackend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private DbContext db;
+
+        public PaymentController(DbContext db)
+        {
+            this.db = db;
+        }
+
+        [Route("GetPayments/{walletId:int}")]
+        [HttpGet]
+        public Payment[] GetWalletPayments(int walletId)
+        {
+            Payment[] payments = db.Payments.Where(
+                payment => payment.WalletId == walletId)
+                .ToArray();
+            return payments;
+        }
+
+        [Route("AddPayment")]
+        [HttpPost]
+        public async Task AddPayment(AddPaymentDto paymentDto)
+        {
+            Payment payment = new Payment()
+            {
+                WalletId = paymentDto.WalletId,
+                Currency = paymentDto.Currency,
+                Description = paymentDto.Description,
+                Value = paymentDto.Value,
+                Wallet = db.Wallets.Where(wallet => wallet.Id == paymentDto.WalletId).First(),
+            };
+
+            await db.Payments.AddAsync(payment);
+            db.SaveChanges();
+        }
+
+        [Route("DeletePayment/{id:int}")]
+        [HttpDelete]
+        public void DeletePayment(int paymentId)
+        {
+            Payment payment = db.Payments.Where(payment => payment.Id == paymentId).First();
+            db.Payments.Remove(payment);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ ViruBackend/Migrations/20231027173802_PaymentMigrationFix.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ViruBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentMigrationFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payment_Wallets_WalletId",
+                table: "Payment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payment",
+                table: "Payment");
+
+            migrationBuilder.RenameTable(
+                name: "Payment",
+                newName: "Payments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payment_WalletId",
+                table: "Payments",
+                newName: "IX_Payments_WalletId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Wallets_WalletId",
+                table: "Payments",
+                column: "WalletId",
+                principalTable: "Wallets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Wallets_WalletId",
+                table: "Payments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments");
+
+            migrationBuilder.RenameTable(
+                name: "Payments",
+                newName: "Payment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payments_WalletId",
+                table: "Payment",
+                newName: "IX_Payment_WalletId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payment",
+                table: "Payment",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payment_Wallets_WalletId",
+                table: "Payment",
+                column: "WalletId",
+                principalTable: "Wallets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
