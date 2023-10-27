d97c9cfb5c893101781f6efaf417d85a2f2862a7
kamil paczkowski <pivotguy888@gmail.com>
2023-10-27 19:08:23 +0200

    Add Payment model

---
+++ ViruBackend/Migrations/20231027170602_PaymentMigration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ViruBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Currency = table.Column<string>(type: "text", nullable: true),
+                    Value = table.Column<int>(type: "integer", nullable: false),
+                    WalletId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payment_Wallets_WalletId",
+                        column: x => x.WalletId,
+                        principalTable: "Wallets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_WalletId",
+                table: "Payment",
+                column: "WalletId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payment");
+        }
+    }
+}
--- ViruBackend/Models/Wallet.cs
+++ ViruBackend/Models/Wallet.cs
@@ -8,5 +8,6 @@ namespace ViruBackend.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public DateTime Created { get; set; }
+        public ICollection<Payment>? Payments { get; set; }
     }
 }
