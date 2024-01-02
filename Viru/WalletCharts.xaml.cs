53f8cf741c4037daee655fdf63bc4117921753e3
Avngarde <pivotguy888@gmail.com>
2024-01-02 23:58:03 +0100

    Add payment types summary

--- Viru/WalletCharts.xaml.cs
+++ Viru/WalletCharts.xaml.cs
@@ -10,6 +10,11 @@ public partial class WalletCharts : ContentPage
     List<int> availableYears;
     List<int> availableMonths;
     PaymentDto[] payments;
+    PaymentTypeDto[] paymentTypes;
+
+    bool expensesToggled = false;
+
+    List<PaymentTypeSummaryModel> paymentTypesSummaries = new();
 
     public WalletCharts(int walletId, string walletName, List<int> availableYears, List<int> availableMonths, PaymentDto[] payments)
 	{
@@ -20,4 +25,72 @@ public partial class WalletCharts : ContentPage
         this.payments = payments;
 		InitializeComponent();
     }
+
+    protected override async void OnAppearing()
+    {
+        await InitPaymentTypes();
+        SummarizeTypes();
+    }
+
+    private async Task InitPaymentTypes()
+    {
+        PaymentTypeService paymentTypeService = new();
+        paymentTypes = await paymentTypeService.GetPaymentTypes(WalletId);
+    }
+
+    private void SummarizeTypes()
+    {
+        paymentTypesSummaries.Clear();
+        summaryListView.ItemsSource = null;
+        float sumTotal = expensesToggled ? payments.Where(x => x.Value < 0).Sum(x => x.Value) : payments.Where(x => x.Value > 0).Sum(x => x.Value);
+        foreach(PaymentTypeDto type in paymentTypes)
+        {
+            float typePaymentsValueSum = 0;
+            if (expensesToggled)
+            {
+                typePaymentsValueSum = payments
+                    .Where(x => x.PaymentTypeId == type.Id && x.Value < 0)
+                    .Sum(x => x.Value);
+            }
+            else 
+            {
+                typePaymentsValueSum = payments
+                    .Where(x => x.PaymentTypeId == type.Id && x.Value >= 0)
+                    .Sum(x => x.Value);                
+            }
+
+            paymentTypesSummaries.Add(new PaymentTypeSummaryModel() 
+            { 
+                TypeName = type.Name,
+                SumPercent = typePaymentsValueSum / sumTotal,
+                SumPercentLabel = $"{Math.Round((typePaymentsValueSum / sumTotal)*100)}%",
+                TypeColor = Color.FromRgba(type.Color)
+            });
+        }
+        paymentTypesSummaries = paymentTypesSummaries.OrderByDescending(type => type.SumPercent).ToList();
+        summaryListView.ItemsSource = paymentTypesSummaries;    
+    }
+
+    private void typeSwitch_OnToggled(object sender, EventArgs e)
+    {
+        if (typeSwitch.IsToggled)
+        {
+            typeSwitch.ThumbColor = Colors.Red;
+            expensesToggled = true;
+        }
+        else
+        {
+            typeSwitch.ThumbColor = Colors.Green;
+            expensesToggled = false;
+        }
+        SummarizeTypes();
+    }
+
+    public class PaymentTypeSummaryModel
+    {
+		public string TypeName { get; set; }
+        public string SumPercentLabel { get; set; }
+		public float SumPercent { get; set; }
+		public Color TypeColor { get; set; }
+	}
 }
